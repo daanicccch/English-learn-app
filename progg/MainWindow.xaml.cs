dda859b12e3645d2add3cf2212548b352a182722
daanicccch <danila.lonsky@ya.ru>
2023-12-09 00:55:01 +0300

    Добвить меню, фикс прогрессбара

--- progg/MainWindow.xaml.cs
+++ progg/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
 
 namespace progg
 {
@@ -22,47 +26,71 @@ namespace progg
     {
         private Button[] answerButtons;
         private Random random = new Random();
+        System.Windows.Controls.Image[] imageArray;
         public MainWindow()
         {
             InitializeComponent();
             answerButtons = new Button[] { button1, button2, button4 };
+            imageArray = new System.Windows.Controls.Image[] {i1, i2, i3, i4, i5, i6, i7, i8, i9, i10};
         }
-        private int currentStage = -1;
+        private int currentStage = 0;
         
         private string[,] wordArray = new string[,]
         {
             { "Picture", "Pucture", "Pictur", "Pecture" },
             { "Mouse", "Muose", "Mous", "Mause" },
             { "Pencil", "Pensil", "Pancil", "Pencel" },
+            { "Penis", "Pinis", "Pins", "Penes" },
+            { "Sunset", "Sunet", "Suset", "Sunsett" },
+            { "Coffee", "Cofee", "Cofe", "Coffe" },
+            { "Guitar", "Gitar", "Gutiar", "Guitarr" },
+            { "Rainbow", "Ranboww", "Rainbo", "Reinbow" },
+            { "Bookshelf", "Bookself", "Bookshelv", "Bokshelf" },
+            { "Elephant", "Elefant", "Elephent", "Elephat" }
         };
       
     private void button_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            if(button.Name == "trueButton")
+            button1.IsEnabled = false;
+            button2.IsEnabled = false;
+            button4.IsEnabled = false;
+            trueButton.IsEnabled = false;
+            if (button.Name == "trueButton")
             {
                 button.Foreground = Brushes.Green;
                 button.Background = new SolidColorBrush(Color.FromRgb(150, 181, 151));
                 button.BorderBrush = new SolidColorBrush(Color.FromRgb(47, 107, 10));
                 nextButton.Visibility = Visibility.Visible;
+                imageArray[currentStage].Source = BitmapFrame.Create(new Uri("D:\\c#\\progg\\progg\\resourse\\greenBar.png"));
             }
             else
             {
                 button.Foreground = Brushes.Red;
                 button.Background = new SolidColorBrush(Color.FromRgb(173, 111, 126));
                 button.BorderBrush = new SolidColorBrush(Color.FromRgb(79, 13, 28));
+                imageArray[currentStage].Source = BitmapFrame.Create(new Uri("D:\\c#\\progg\\progg\\resourse\\redBar.png"));
+                nextButton.Visibility = Visibility.Visible;
+
             } 
         }
+
+
         private void UpdateUIForCurrentStage()
         {
             
             if (currentStage < wordArray.Length)
             {
-               
-                trueButton.Content = wordArray[currentStage, 0];
-                button1.Content = wordArray[currentStage, 1];
-                button2.Content = wordArray[currentStage, 2];
-                button4.Content = wordArray[currentStage, 3];
+
+                tblocktrue.Text = wordArray[currentStage, 0];
+                tblock1.Text = wordArray[currentStage, 1];
+                tblock2.Text = wordArray[currentStage, 2];
+                tblock4.Text = wordArray[currentStage, 3];
+
+                button1.IsEnabled = true;
+                button2.IsEnabled = true;
+                button4.IsEnabled = true;
+                trueButton.IsEnabled = true;
 
                 RestoreButton(trueButton);
                 RestoreButton(button1);
@@ -76,9 +104,9 @@ namespace progg
         }
         private void RestoreButton(Button button) // очистка кнопок
         {
-            button.Foreground = Brushes.Black;
-            button.Background = new SolidColorBrush(Color.FromRgb(184, 215, 245)); 
-            button.BorderBrush = new SolidColorBrush(Color.FromRgb(74, 131, 217)); 
+            button.Foreground = new SolidColorBrush(Color.FromRgb(171, 171, 171)); 
+            button.Background = new SolidColorBrush(Color.FromRgb(46,46,46)); 
+            button.BorderBrush = new SolidColorBrush(Color.FromRgb(33, 33, 33)); 
         }
         private void nextButton_Click(object sender, RoutedEventArgs e)
         {
@@ -97,6 +125,17 @@ namespace progg
             Grid.SetRow(randomButton, tempRow);
             currentStage++;
             UpdateUIForCurrentStage();
+            nextButton.Visibility = Visibility.Collapsed;
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (((MenuItem)sender).Header.ToString()=="Home")
+            {
+                HomeUserControl homeUserControl = new HomeUserControl();
+                Content = homeUserControl;
+            }
+        }
+
     }
 }
